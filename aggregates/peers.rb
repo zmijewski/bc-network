2df406b39bb344197db71bdab63075c7ca065dd1
Piotr Żmijewski <zmijewski.developer@gmail.com>
2021-04-06 17:35:49 +0200

    Extract peers to separate aggregate (#2)

---
+++ aggregates/peers.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Aggregates
+  class Peers
+    extend Dry::Initializer
+
+    option :owner
+    option :source, default: proc { ::Concurrent::Set.new }
+
+    def create(peers)
+      if peers.is_a? Enumerable
+        source.merge(peers)
+        source.delete(owner)
+      else
+        source.add(peers)
+      end
+    end
+
+    def delete(peers)
+      if peers.is_a? Enumerable
+        source.subtract(peers)
+      else
+        source.delete(peers)
+      end
+    end
+
+    def peers
+      source
+    end
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -10,15 +10,14 @@ require 'logger'
 
 require './value_objects/peer'
 require './protocols/tcp'
+require './aggregates/peers'
 
 LOGGER = Logger.new($stdout)
 
 case $BC_NODE
 when :discovery_node
-  require './nodes/discovery/config'
   require './nodes/discovery/server'
 when :full_node
-  require './nodes/discovery/config'
   require './nodes/full/client'
   require './nodes/full/server'
 end
