d2c442d7a05069549bb48a496ffa17391186a11e
Piotr Żmijewski <zmijewski.developer@gmail.com>
2021-11-24 22:19:19 +0100

    Reduced dependency on transactions service

--- blockchain/services/transactions.rb
+++ blockchain/services/transactions.rb
@@ -2,6 +2,12 @@
 
 module Services
   class Transactions
+<<<<<<< HEAD
+=======
+    extend Dry::Initializer
+
+    option :owner
+>>>>>>> a09dfba... Reduced dependency on transactions service
 
     def create(from:, to:, blockchain:)
       amount = [rand(1..blockchain.compute_balances[from.public_key]), 500].min
@@ -12,5 +18,17 @@ module Services
         private_key: from.private_key
       )
     end
+<<<<<<< HEAD
+=======
+
+    def create_public_transaction(request)
+      ::Transaction.new(
+        from: request.params[:from],
+        to: request.params[:to],
+        amount: request.params[:amount],
+        private_key: owner.private_key
+      )
+    end
+>>>>>>> a09dfba... Reduced dependency on transactions service
   end
 end
